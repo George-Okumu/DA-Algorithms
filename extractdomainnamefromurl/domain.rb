1d52f3f8f39b5e2547afdde104994f908db47cc4
George-Okumu <gokumu12@gmail.com>
2023-01-24 11:24:53 +0300

    domain url

---
+++ extractdomainnamefromurl/domain.rb
@@ -0,0 +1,19 @@
+def domain_name(url)
+    # return just the domain name
+    if(url.include? "http")
+      host_name = URI.parse(url).host.split(".");
+      # delete www if incase exists
+      domain_name = host_name -= %w{www}
+      print domain_name
+      domain_name[0]
+    elsif url.include? "www"
+      url.split(".")[1]
+    else
+      url.split(".")[0]
+    end
+  end
+
+  # Refactored solution
+  def domain_name(url)
+    URI.parse(url).host.split('.').last(2)[0]
+  end
